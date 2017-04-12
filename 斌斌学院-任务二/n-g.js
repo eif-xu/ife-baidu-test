614dd997912a474b88fb052b2c5977b6bec2102f
徐珉强 <1012416749@qq.com>
2017-04-12 23:55:46 +0800

    。。。

--- 斌斌学院-任务二/n-g.js
+++ 斌斌学院-任务二/n-g.js
@@ -1,22 +1,35 @@
-window.onload=function(){
-
-	var apiDate=[
-		["北京",90],
-		["上海",50],
-		["福州",10],
-		["杭州",50],
-		["成都",90],
-		["西安",100]
+var apiDate=[
+		["北京", 90],
+		["上海", 50],
+		["福州", 10],
+		["杭州", 50],
+		["成都", 90],
+		["西安", 100]
 	];
-
 (function(){
-	 /*
-  在注释下方编写代码
-  遍历读取aqiData中各个城市的数据
-  将空气质量指数大于60的城市显示到aqi-list的列表中
-  */
-  
-  
+			/*
+	  在注释下方编写代码
+	  遍历读取aqiData中各个城市的数据
+	  将空气质量指数大于60的城市显示到aqi-list的列表中
+	  */
+
+	var len=apiDate.length;
+	var box=[];
+	var lg=box.length;
+	var nwlist=document.getElementById('api-list');
+	// 超过60的城市选出
+	for (var i=0;i<=len;i++) {
+		if (apiDate[i][1]>60){
+			console.log('!!!!!!');
+			box.push([apiDate[i][0],apiDate[i][1]])
+		}
+	}
+	console.log(box);
+
+	// 把超过60的城市插入
+	for (var j=0;j<=lg;i++) {
+		nwlist.innerHTML='<li>'+box[j]+'</li>'
+	}
+
 })();
 
-}
